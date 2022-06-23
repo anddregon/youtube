301889321955c2e541feabd2c91556b624accafa
Felipe Gonzalez <xfelipe05@gmail.com>
2022-06-23 17:27:12 -0500

    Prueba primalidad

--- 004/prueba_primalidad.py
+++ 004/prueba_primalidad.py
@@ -2,25 +2,28 @@
 #* Si p es primo:
 #* (p-1)! = p° - 1 
 
+
 def factorial(n):
     fact = 1
     if n < 0:
         return 0
-    elif n == 0: 
+    elif n == 0:
         return 1
     while (n > 1):
         fact *= n #* fact = fact * n
         n -= 1 #* n = n - 1
     return fact
+ 
 
-def main():
-    
+def main ():
+
     numero = int(input('Digite un número: '))
     wilson = factorial(numero - 1) + 1
     if wilson % numero == 0:
         print('El número es primo')
-    else: 
+    else:
         print('El número no es primo')
+
 
 if __name__ == '__main__':
     main()
