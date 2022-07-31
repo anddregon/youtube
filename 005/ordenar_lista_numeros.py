1de5c7cf9bd34a80a54fb5954f827ba864bd5f65
Felipe Gonzalez <xfelipe05@gmail.com>
2022-07-31 02:58:08 -0500

    Ejercicio 005 youtube

--- 005/ordenar_lista_numeros.py
+++ 005/ordenar_lista_numeros.py
@@ -4,16 +4,16 @@ def main():
 
     lista = []
 
-    for indice in range (1,21):
+    for indice in range(1,21):
         lista.append(random.randint(1,10))
-    
+
     #Lista desordenada
     print(lista)
-    
-    #Ordenarmos la lista
-    lista.sort()
 
-    #Recorremos la lista ordenada
+    #Ordenar lista
+    lista.sort(reverse = True)
+
+    #Recorrer la lista ordenada
     print(lista)
 
 if __name__ == '__main__':
